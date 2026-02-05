c86fa10195d9888db7c92ebab54c94dfee7cbb75
mossalahhh <ms4407025@gmail.com>
2026-02-06 01:42:06 +0200

    fix(authorization): update error code for unauthorized access from 401 to 403

--- Back-End/src/middleware/authorization.js
+++ Back-End/src/middleware/authorization.js
@@ -3,7 +3,7 @@ import { catchErorr } from "../utils/catchErorr.js";
 export const isAuthorized = (role) => {
   return catchErorr(async (req, res, next) => {
     if (role !== req.user.role) {
-      return next(new Error("You're Not authorized", { cause: 401 }));
+      return next(new Error("You're Not authorized", { cause: 403 }));
     }
     return next();
   });
