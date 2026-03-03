d3443b1cc11a026814b63fba8ef98f251a2ad099
mossalahhh <ms4407025@gmail.com>
2026-03-03 23:15:52 +0200

    feat(multer):add multer configuration for image file uploads

---
+++ Back-End/src/utils/multer.js
@@ -0,0 +1,24 @@
+import multer, { diskStorage } from "multer";
+
+const validationImg = {
+  images: [
+    "image/apng",
+    "image/avif",
+    "image/bmp",
+    "image/vnd.microsoft.icon",
+    "image/jpeg",
+    "image/png",
+    "image/svg+xml",
+    "image/webp",
+  ],
+};
+
+export const fileUpload = (filterArray) => {
+  const fileFilter = (req, res, cb) => {
+    if (!filterArray.includes(fileFilter.mimetype)) {
+      return cb(new Error("invalid img type"), false);
+    }
+    return cb(null, true);
+  };
+  return multer({ storage: diskStorage({}), fileFilter });
+};
