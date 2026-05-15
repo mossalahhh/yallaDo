c726a950d58ea5fe4f0eedb8ef9fd44ef5fdffc7
mossalahhh <ms4407025@gmail.com>
2026-05-15 16:09:56 +0300

    fix(index): set default port to 3000 if not specified in environment variables

--- Back-End/index.js
+++ Back-End/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 appRouter(app, express);
 connectDB();
 app.listen(port, () => console.log(`Server running on port ${port}!`));
