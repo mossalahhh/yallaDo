438c4775be2c46d31c116740a4963a5c947b98e8
mossalahhh <ms4407025@gmail.com>
2026-06-27 01:32:15 +0300

    fix(cors): update localhost entry in CORS whitelist

--- Back-End/src/appRouter.js
+++ Back-End/src/appRouter.js
@@ -32,7 +32,7 @@ export const appRouter = (app, express) => {
     "http://localhost:61943",
     "http://localhost:3000",
     "http://localhost:50780",
-    "http://localhost:59305",
+    "http://localhost:59913",
   ];
   app.use((req, res, next) => {
     //handle requests from front-end
