214c32e9c1aeba3199ca4498619c24fa1ccb2389
mossalahhh <ms4407025@gmail.com>
2026-06-28 17:50:09 +0300

    fix(cors): enable CORS middleware for handling cross-origin requests

--- Back-End/src/appRouter.js
+++ Back-End/src/appRouter.js
@@ -7,45 +7,45 @@ import taskRouter from "./modules/task/task_router.js";
 import rewardRouter from "./modules/reward/reward_router.js";
 import notificationsRouter from "./modules/notifications/notifications_router.js";
 import aiRouter from "./modules/ai/ai_router.js";
-// import cors from "cors";
+import cors from "cors";
 
 export const appRouter = (app, express) => {
   if (process.env.NODE_ENV === "dev") {
     app.use(morgan("common"));
   }
 
-  // app.use(
-  //   cors({
-  //     origin: true,
-  //     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  //     allowedHeaders: ["Content-Type", "Authorization"],
-  //     credentials: false,
-  //   }),
-  // );
+  app.use(
+    cors({
+      origin: true,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: false,
+    }),
+  );
 
   // undefined for postman requests
   // null for fs
-  const whiteList = [
-    undefined,
-    null,
-    "http://127.0.0.1:5500",
-    "http://localhost:61943",
-    "http://localhost:3000",
-    "http://localhost:50780",
-    "http://localhost:60726",
-  ];
-  app.use((req, res, next) => {
-    //handle requests from front-end
-    if (!whiteList.includes(req.header("origin"))) {
-      return next(new Error("Blocked By CORS"));
-    }
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    //to allow request for localHost
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    return next();
-  });
+  // const whiteList = [
+  //   undefined,
+  //   null,
+  //   "http://127.0.0.1:5500",
+  //   "http://localhost:61943",
+  //   "http://localhost:3000",
+  //   "http://localhost:50780",
+  //   "http://localhost:60726",
+  // ];
+  // app.use((req, res, next) => {
+  //   //handle requests from front-end
+  //   if (!whiteList.includes(req.header("origin"))) {
+  //     return next(new Error("Blocked By CORS"));
+  //   }
+  //   res.setHeader("Access-Control-Allow-Origin", "*");
+  //   res.setHeader("Access-Control-Allow-Methods", "*");
+  //   res.setHeader("Access-Control-Allow-Headers", "*");
+  //   //to allow request for localHost
+  //   res.setHeader("Access-Control-Allow-Private-Network", true);
+  //   return next();
+  // });
 
   //parse express data
   app.use(express.json());
