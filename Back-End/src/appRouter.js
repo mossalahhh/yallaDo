7b245cb05d3574ddf72250da8da55ff59b961b10
mossalahhh <ms4407025@gmail.com>
2026-06-30 19:36:41 +0300

    fix(appRouter): update localhost whitelist to correct port 56450

--- Back-End/src/appRouter.js
+++ Back-End/src/appRouter.js
@@ -30,7 +30,7 @@ export const appRouter = (app, express) => {
     null,
     "http://127.0.0.1:5500",
     "http://localhost:3000",
-    "http://localhost:54551",
+    "http://localhost:56450",
   ];
   app.use((req, res, next) => {
     //handle requests from front-end
